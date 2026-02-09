34381ca0b86b759e2abc241c591f74159d4fd4d5
alicorak0 <ali_123-fb@hotmail.com>
2026-02-09 14:32:23 +0300

    9 Şubat 14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,48 @@
-import {  RouterModule,Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./component/header-component/header-component";
 import { ContactComponent } from './component/contact-component/contact-component';
 import { MainmenuComponent } from './component/mainmenu-component/mainmenu-component';
 import { ProductComponent } from "./component/product-component/product-component";
 import { MenuComponent } from "./component/menu-component/menu-component";
+import { CategoriesComponent } from "./component/categories-component/categories-component";
 
 export const routes: Routes = [
   //  { path: '', redirectTo: 'anasayfa', pathMatch: 'full' },
-   // { path: 'categories/:categoryName', component: WheelsComponent },
- 
-    //  { path: 'anasayfa', component: MainmenuComponent },
-          { path: '', component: MainmenuComponent, pathMatch: 'full' },
+  // { path: 'categories/:categoryName', component: WheelsComponent },
 
-     { path: 'iletisim', component: ContactComponent },
-     { path: 'menu', component: MenuComponent },
-{ path: 'menu/:name', component: ProductComponent }
-      
-  
+  //  { path: 'anasayfa', component: MainmenuComponent },
+  { path: '', component: MainmenuComponent, pathMatch: 'full' },
+
+  { path: 'iletisim', component: ContactComponent },
+  {
+    path: 'menu', component: MenuComponent, children: [
+      {
+        path: '',
+        component: CategoriesComponent   // 👈 /menu açılınca burası render olur
+      },
+      {
+        path:':name',component: ProductComponent   // 👈 /menu/burgers, /menu/snacks vs açılınca burası render olu  r
+      }
+    ]
+  }
+
+
+
+
 ];
 
 @NgModule({
 
-imports: [
-  RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'disabled',   // scroll'u kapatır
-    anchorScrolling: 'disabled'              // anchor scroll'u kapatır
-  })
-],    exports:[RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'disabled',   // scroll'u kapatır
+      anchorScrolling: 'disabled'              // anchor scroll'u kapatır
+    })
+  ], exports: [RouterModule]
 })
 
-export class  AppRoutingModule{
+export class AppRoutingModule {
 
 }
 
---
+++ src/app/component/categories-component/categories-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import{ CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgModule } from '@angular/core';
+@Component({
+  selector: 'app-categories-component',
+  imports: [CommonModule,RouterModule],
+  templateUrl: './categories-component.html',
+  styleUrl: './categories-component.css',
+})
+export class CategoriesComponent {
+
+}
